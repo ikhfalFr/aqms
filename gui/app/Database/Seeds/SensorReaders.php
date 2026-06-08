c92367e868b3a2c7dbf00bf7bc4edf80dbbfe252
ikhfalFr <95295673+ikhfalFr@users.noreply.github.com>
2026-06-08 19:49:05 +0700

    Add pm_alphasense.py sensor reader to seed data

--- gui/app/Database/Seeds/SensorReaders.php
+++ gui/app/Database/Seeds/SensorReaders.php
@@ -12,6 +12,7 @@ class SensorReaders extends Seeder
 		$data = [
 			['driver' => 'mainboard_efs2.alpha.py', 'sensor_code' => '/dev/ttyMAINBOARD', 'baud_rate' => '9600', 'pins' => null],
 			['driver' => 'hc.usb.py', 'sensor_code' => '/dev/ttyUSBHC', 'baud_rate' => '9600', 'pins' => null],
+			['driver' => 'pm_alphasense.py', 'sensor_code' => '/dev/ttyPM', 'baud_rate' => '9600', 'pins' => null],
 		];
 		$this->db->table('sensor_readers')->insertBatch($data);
 	}
