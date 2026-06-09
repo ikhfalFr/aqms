6043d6e8ffde254bda905a2a466e585fec521bf3
ikhfalFr <ikhfalfauziarahman10@gmail.com>
2026-06-09 09:36:33 +0700

    Kosongkan seluruh file di branch

--- aqms_start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Reading devices... Please wait!!"
-sleep 1s
-ls /dev/ttyUSB*
-ls /dev/ttyD*
-ls /dev/ttyM*
-cd ~/aqmsFS2/ && python3 aqms_start.py
-$SHELL
--- drivers/db_connect.py
+++
@@ -1,8 +0,0 @@
-from mysql.connector.constants import ClientFlag
-import mysql.connector
-
-def connecting():
-    try:
-        return mysql.connector.connect(host="localhost",user="root",passwd="R2h2s12*",database="aqms_fs2")
-    except Exception as e: 
-        return False
